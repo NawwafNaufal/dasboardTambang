b5b2a8628138079ec0f67341c6e7945dd6de24d1
NawwafNaufal <nawwafnaufal03@gmail.com>
2025-12-31 09:07:49 +0700

    session key

--- backend/src/config/session.ts
+++ backend/src/config/session.ts
@@ -3,7 +3,7 @@ import "dotenv/config"
 
 export const sessionConfig = session ({
     name : "sid",
-    secret : process.env.SESSION_SECRET || "secertedfd",
+    secret : process.env.SESSION_SECRET as string,
     resave : false,
     saveUninitialized : false,
     rolling : true,
