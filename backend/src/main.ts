3f858d88fccf720c2ae6d66779f29ae329a35029
Nawwaf Naufal <nawwafnaufal03@gmail.com>
2026-02-20 08:37:19 +0700

    revisi

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT;
 
 
 app.use(cors({
-  origin: ["http://localhost:4000", "http://localhost:5173"],
+  origin: ["http://76.13.198.60:4000", "http://76.13.198.60:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"]
