26495e104846fc0f94ac30e23b2f78f4acd702df
Nawwaf Naufal <nawwafnaufal03@gmail.com>
2026-02-23 10:47:27 +0700

    fix bug plan rkpa 18 to 18.000

--- backend/src/service/productivity/planRkpaProductivity.ts
+++ backend/src/service/productivity/planRkpaProductivity.ts
@@ -4,7 +4,6 @@ import { SiteRkpaData } from "../../interface/productivity/monthlyActualType";
 
 export async function getPlanRkpaByTodayService() {
   const today = todayISO();
-
   const records = await DailyOperation.find({ date: today })
     .select("site activities")
     .lean();
@@ -20,16 +19,16 @@ export async function getPlanRkpaByTodayService() {
 
   for (const record of records) {
     siteData[record.site] ??= { activities: [] };
-
     const activities = record.activities ?? {};
 
     for (const [key, value] of Object.entries<any>(activities)) {
+      const isPadang = record.site === 'Padang';
+
       siteData[record.site].activities.push({
         activityName: key,
-        planRevenue: value.plan || 0,
+        planRevenue: isPadang ? (value.plan || 0) * 1000 : (value.plan || 0),
         rkpaRevenue: value.rkap || 0,
         actual: value.actual || 0,
-
         planRevenueChange: Number((Math.random() * 30 - 10).toFixed(2)),
         rkpaRevenueChange: Number((Math.random() * 30 - 10).toFixed(2)),
       });
