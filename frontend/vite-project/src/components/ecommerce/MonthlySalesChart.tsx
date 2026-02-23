595293a034615b0493b58f29363184d7f1d21728
Nawwaf Naufal <nawwafnaufal03@gmail.com>
2026-02-23 11:44:57 +0700

    change formater

--- frontend/vite-project/src/components/ecommerce/MonthlySalesChart.tsx
+++ frontend/vite-project/src/components/ecommerce/MonthlySalesChart.tsx
@@ -283,7 +283,7 @@ export default function MonthlySalesChart({
     },
     dataLabels: {
       enabled: true,
-      formatter: (val: number) => val.toFixed(2),
+      formatter: (val: number) => val.toLocaleString('id-ID'),
       offsetY: -18,
       style: {
         fontSize: '11px',
@@ -333,7 +333,7 @@ export default function MonthlySalesChart({
         show: false,
       },
       y: {
-        formatter: (val: number) => `${val}`,
+        formatter: (val: number) => val.toLocaleString('id-ID'),
       },
     },
   };
